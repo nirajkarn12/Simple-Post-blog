c3cb2cf58fb758a54da7e6748114c7c05cd0334e
niraj karna <149599528+nirajkarn12@users.noreply.github.com>
2024-03-15 11:45:04 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SwaggerController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,7 +27,17 @@ Route::middleware('auth')->group(function () {
 Route::get('/admin/post/list', [PostController::class, 'view'])->name('post.view');
 Route::get('/admin/post/create', [PostController::class, 'create'])->name('post.create');
 Route::post('/admin/post/create', [PostController::class, 'store'])->name('post.store');
+Route::get('/admin/post/edit/{postid}', [PostController::class, 'edit'])->name('post.edit');
+Route::put('/admin/post/edit/{postid}', [PostController::class, 'update'])->name('post.update');
+Route::delete('/admin/post/delete/{postid}', [PostController::class, 'destroy'])->name('post.destroy');
+Route::get('/admin/post/comment/{postid}', [PostController::class, 'comment'])->name('post.comment');
 
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/api/documentation', function () {
+    return view('vendor.l5-swagger.index');
+});
+
+Route::get('/api/documentation', [SwaggerController::class, 'index']);
